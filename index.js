6e2ff2a9397eef2da84cf099b004a3d0bbfbadcf
deldon <r.deldon@gmail.com>
2022-05-24 00:05:32 +0200

    refacto and fix bug

--- index.js
+++ index.js
@@ -6,145 +6,131 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 });
 
 const random_hex_color_code = () => {
-  let n = (Math.random() * 0xfffff * 1000000).toString(16);
-  return '#' + n.slice(0, 6);
+    let n = (Math.random() * 0xfffff * 1000000).toString(16);
+    return '#' + n.slice(0, 6);
 };
 let player = []
 
 const sizeMap = {
-  x: 34,
-  y: 25
+    x: 34,
+    y: 25
 }
-
 
 io.on('connection', (socket) => {
 
-
-
-
-  // creation du player
-
-  const newPlayer = {
-    id: socket.id,
-    name: 'player',
-    color: random_hex_color_code(),
-    direction: 0,
-    x: 0,
-    y: 0
-  }
-  player.push(newPlayer);
-  console.log('new player: ', player);
-  io.emit('chat message', player);
-
-
-
-
-
-  socket.on('chat message', (msg) => {
-
-
-    const myPlayerId = (element) => element.id == socket.id;
-    const persoId = player.findIndex(myPlayerId)
-    const vitesse = 1;
-
-    if (msg == 'right') {
-      if (player[persoId].x < sizeMap.x) {
-        // console.log((player[persoId].x + vitesse) == objs.x);
-        const free = player.some((objs) => {
-
-          if ((player[persoId].x + vitesse) == objs.x && player[persoId].y == objs.y) {
-            return true
-          }
-          return false
-        })
-        if (!free) {
-          player[persoId].x = player[persoId].x + vitesse;
-          //player[persoId].direction = 64;
-        }
-      }
-      player[persoId].direction = 64;
-
+    const newPlayer = {
+        id: socket.id,
+        name: 'player',
+        color: random_hex_color_code(),
+        direction: 0,
+        x: 0,
+        y: 0
     }
-    if (msg == 'left') {
-      if (player[persoId].x > 0) {
-
-
-        const free = player.some((objs) => {
-
-          if ((player[persoId].x - vitesse) == objs.x && player[persoId].y == objs.y) {
-            return true
-          }
-          return false
-        })
-        if (!free) {
-          player[persoId].x = player[persoId].x - vitesse;
-          
-        }
-      }
-      player[persoId].direction = 32
-    }
-    if (msg == 'up') {
-      if (player[persoId].y > 0) {
-
-        const free = player.some((objs) => {
-
-          if ((player[persoId].y - vitesse) == objs.y && player[persoId].x == objs.x) {
-            return true
-          }
-          return false
-        })
-        if (!free) {
-          player[persoId].y = player[persoId].y - vitesse;
-          
-        }
-
-
-
-      }
-      player[persoId].direction = 96;
-    }
-    if (msg == 'down') {
-      if (player[persoId].y < sizeMap.y) {
-
-        const free = player.some((objs) => {
-
-          if ((player[persoId].y + vitesse) == objs.y && player[persoId].x == objs.x) {
-            return true
-          }
-          return false
-        })
-        if (!free) {
-          player[persoId].y = player[persoId].y + vitesse;
-          
-        }
-
-
-      }
-      player[persoId].direction = 0;
-    }
-
-    io.emit('chat message', player);
-    // console.log(player);
-
-
-    // console.log('message: ' + msg);
-  });
-
-  socket.on('disconnect', () => {
-    const myPlayerId = (element) => element.id == socket.id;
-    const persoId = player.findIndex(myPlayerId)
-
-    player.splice(persoId, 1);
+    player.push(newPlayer);
+    console.log('new player: ', newPlayer.id);
     io.emit('chat message', player);
 
-  });
+
+    socket.on('chat message', (msg) => {
+
+        const myPlayerId = (element) => element.id == socket.id;
+        const persoId = player.findIndex(myPlayerId)
+        const vitesse = 1;
+
+        if (msg == 'right') {
+            if (player[persoId].x < sizeMap.x) {
+
+                const free = player.some((objs) => {
+
+                    if ((player[persoId].x + vitesse) == objs.x && player[persoId].y == objs.y) {
+                        return true
+                    }
+                    return false
+                })
+                if (!free) {
+                    player[persoId].x = player[persoId].x + vitesse;
+                    //player[persoId].direction = 64;
+                }
+            }
+            player[persoId].direction = 64;
+
+        }
+        if (msg == 'left') {
+            if (player[persoId].x > 0) {
+
+
+                const free = player.some((objs) => {
+
+                    if ((player[persoId].x - vitesse) == objs.x && player[persoId].y == objs.y) {
+                        return true
+                    }
+                    return false
+                })
+                if (!free) {
+                    player[persoId].x = player[persoId].x - vitesse;
+
+                }
+            }
+            player[persoId].direction = 32
+        }
+        if (msg == 'up') {
+            if (player[persoId].y > 0) {
+
+                const free = player.some((objs) => {
+
+                    if ((player[persoId].y - vitesse) == objs.y && player[persoId].x == objs.x) {
+                        return true
+                    }
+                    return false
+                })
+                if (!free) {
+                    player[persoId].y = player[persoId].y - vitesse;
+
+                }
+
+            }
+            player[persoId].direction = 96;
+        }
+        if (msg == 'down') {
+            if (player[persoId].y < sizeMap.y) {
+
+                const free = player.some((objs) => {
+
+                    if ((player[persoId].y + vitesse) == objs.y && player[persoId].x == objs.x) {
+                        return true
+                    }
+                    return false
+                })
+                if (!free) {
+                    player[persoId].y = player[persoId].y + vitesse;
+
+                }
+
+            }
+            player[persoId].direction = 0;
+        }
+
+        io.emit('chat message', player);
+
+    });
+
+    socket.on('disconnect', () => {
+        const myPlayerId = (element) => element.id == socket.id;
+        const persoId = player.findIndex(myPlayerId)
+
+        player.splice(persoId, 1);
+        io.emit('chat message', player);
+
+    });
 });
+
 app.use(express.static('./public'));
+
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+    console.log('listening on *:3000');
 });
 
---
+++ public/js/app.js
@@ -0,0 +1,64 @@
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+var socket = io();
+const recSize = 28;
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var canvas2 = document.getElementById("myCanvas2");
+var ctx2 = canvas2.getContext("2d");
+
+//ctx2.clearRect(0, 0, canvas.width, canvas.height);
+for (let i = 0; i < 10; i++) {
+
+    let img = new Image();
+    img.onload = function () {
+        console.log();
+        ctx2.drawImage(img, (90 * getRandomInt(5)) + 90, (90 * getRandomInt(5)) + 90);
+    };
+    img.src = '/img/green.jpeg';
+
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+
+function keyDownHandler(e) {
+    if (e.key == "Right" || e.key == "ArrowRight") {
+
+        socket.emit('chat message', 'right');
+    }
+    if (e.key == "Left" || e.key == "ArrowLeft") {
+
+        socket.emit('chat message', 'left');
+    }
+    if (e.key == "Up" || e.key == "ArrowUp") {
+
+        socket.emit('chat message', 'up');
+    }
+    if (e.key == "Down" || e.key == "ArrowDown") {
+
+        socket.emit('chat message', 'down');
+    }
+}
+
+let img2 = new Image();
+
+socket.on('chat message', function (msg) {
+
+    img2.onload = function () {
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (const i of msg) {
+
+            ctx.drawImage(img2, 0, i.direction, 28, 32, (i.x * recSize), (i.y * recSize), 28, 32);
+        }
+    };
+
+    img2.src = '/img/sprite.png';
+
+});
